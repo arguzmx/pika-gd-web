c109ab0ca266c8bc0b95c081100f7855b47d1a67
Desarrollo Arguz <desarrollo@arguz.com>
2021-04-08 20:07:49 -0500

    AuthServer

--- src/app/@acceso/auth-config.ts
+++ src/app/@acceso/auth-config.ts
@@ -1,5 +1,5 @@
-import { environment } from './../../environments/environment';
 import { AuthConfig } from 'angular-oauth2-oidc';
+import { environment } from '../../environments/environment';
 
 export const authConfig: AuthConfig = {
   issuer: environment.authUrl,
